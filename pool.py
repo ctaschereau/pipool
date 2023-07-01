bf03a3fa40f13849f2c6afeb1bb477d7c90f1ee4
Cédric Taschereau <cedric.taschereau@gmail.com>
2023-07-01 07:11:08 -0400

    Fix config path

--- pool.py
+++ pool.py
@@ -5,6 +5,8 @@ import time
 from flask import Flask, jsonify
 import configparser
 
+# Get the absolute path to the directory of this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Initialize the GPIO Pins
 os.system('modprobe w1-gpio')  # Turns on the GPIO module
@@ -16,8 +18,9 @@ device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 # Read the config file
+config_path = os.path.join(script_dir, 'config.ini')
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(config_path)
 
 # Get the Celsius offset from the config file
 offset = config.getfloat('Temperature', 'Offset')
