de0e4ed394fc42ff4b6f64bdaad22a726ca8e9fc
Cédric Taschereau <ctaschereau@boreal-is.com>
2026-06-05 07:56:14 -0400

    Change local key name

--- index.ts
+++ index.ts
@@ -31,11 +31,11 @@ const port: number = config.port;
 
 const hostname = os.hostname().replace('.local', '');
 let server, isSecure = false;
-// if the hostname starts with "laptop0072", then use the cert my_borealis_cert.cer to create a secure server. Otherwise, create a non-secure server.
+// if the hostname starts with "laptop0072", then use the local cert and key to create a secure server. Otherwise, create a non-secure server.
 if (hostname.startsWith("laptop0072")) {
 	const options = {
-		key: fs.readFileSync('my_borealis_key.key'),
-		cert: fs.readFileSync('my_borealis_cert.cer')
+		key: fs.readFileSync('my_key.key'),
+		cert: fs.readFileSync('my_cert.cer')
 	};
 	server = https.createServer(options, app)
 	isSecure = true;
